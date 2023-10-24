89435244ea3041ee9ef05408ea8963c21cb5b1f4
Cristofer Pizarro <102608682+cristoferapr@users.noreply.github.com>
2023-10-23 21:50:37 -0300

    Update solver_LU.jl

--- solver_LU.jl
+++ solver_LU.jl
@@ -55,7 +55,7 @@ function factorizacion_lu(matriz)
 end
 
 # Función para resolver un sistema de ecuaciones lineales utilizando las matrices LU y el vector de solucion B
-function solver_sistemaEcu(L, U, B)
+function solver_sitemaEcu(L, U, B)
     # Se calcula a, b, c en funcion del vector de solucion B y la matriz L
     a = B[1]
     b = (B[2] - L[2, 1] * a) / L[2, 2]
@@ -64,24 +64,27 @@ function solver_sistemaEcu(L, U, B)
     # Imprimir los resultados intermedios
     println("a = $a, b = $b, c = $c")
 
-    # Calcular x,y,z en función de la matriz U y los datos a, b y c obtenidos anteriormente
-    z = c / U[3, 3]
-    y = (b - z * U[2, 3]) / U[2, 2]
-    x = (a - y * U[1, 2] - z * U[1, 3]) / U[1, 1]
+    S = [a, b, c]
+    # Se resuelve el sistema de ecuaciones haciendo uso de los valores intermedios, la matriz U y el metodo backward_substitution
+    x = backward_substitution(U, S)
+end
 
-    # Imprimir la solución final
-    println("\nSolución:")
-    println("x = $x")
-    println("y = $y")
-    println("z = $z")
+# Función de sustitución hacia atrás
+function backward_substitution(A, b)
+    n = size(A, 1)
+    x = zeros(n)
+    for i = n:-1:1
+        x[i] = (b[i] - sum(A[i, i+1:n] .* x[i+1:n])) / A[i, i]
+    end
+    return x
 end
 
 # Definir el sistema de ecuaciones como una matriz A 3x3 y un vector B
-A = [3.0 2.0 1.0;
-     2.0 8.0 7.0;
-     1.0 7.0 14.0]
-B = [10, 11, 15]
+A = [1.0 2.0 3.0;
+     4.0 5.0 6.0;
+     7.0 8.0 10.0]
+B = [1.0, 1.0, 1.0]
 
 # Realizar la factorización LU y resolver el sistema de ecuaciones
 L, U = factorizacion_lu(A)
-solver_sistemaEcu(L, U, B)
+sustitutionBackward(L, U, B)
